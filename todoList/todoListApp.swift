c6437546f5c21ca6310e12632fc4e19be29ae24a
duda caetano <mariaeduardascaetano@gmail.com>
2024-06-15 15:12:32 -0300

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Maria Eduarda on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
